e089e3707556c52150d56181f1ed14e8eb2c0d6f
Selman GÖKTAŞ <selman@karatekin.edu.tr>
2021-04-15 02:51:47 +0300

    future build kullanılarak film json getirildi

--- lib/ders8/filmOdevi/film-list.dart
+++ lib/ders8/filmOdevi/film-list.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:http/http.dart' as http;
 import 'film-model.dart';
 
@@ -17,19 +16,25 @@ class _FilmViewBuilderState extends State<FilmViewBuilder> {
   getState() async {
     Uri url = Uri.parse(
         'https://api.themoviedb.org/3/tv/airing_today?api_key=79f9638dc1bcf9a4e5a09db68640db20&language=tr-TR&page=1');
-    var responpe = await http.get(url);
-    var decode = json.decode(responpe.body);
-    var filmList = decode['results'];
-    for (var item in filmList) {
-      flist.add(FilmModel.fromJson(item));
-    }
+    final responpe = await http.get(url);
+
+    if (responpe.statusCode == 200) {
+      var decode = json.decode(responpe.body);
+      var filmList = decode['results'];
+      for (var item in filmList) {
+        flist.add(FilmModel.fromJson(item));
+      }
+      //return FilmModel.fromJson(filmList);
+      return flist;
+    } else
+      throw Exception('Failed to load post');
   }
 
-  @override
+  /* @override
   void initState() {
     getState();
     super.initState();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -37,55 +42,92 @@ class _FilmViewBuilderState extends State<FilmViewBuilder> {
     var ekranH = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-      ),
-      drawer: Container(
-        child: Text(''),
-      ),
-      body: StaggeredGridView.countBuilder(
-        crossAxisCount: 4,
-        itemCount: flist.length,
-        itemBuilder: (BuildContext context, int index) => new Column(
+        title: Row(
           children: [
-            new Container(
-              padding: EdgeInsets.all(5),
-              width: ekranW,
-              height: index * 2 / 2 == index ? ekranH * 0.5 : ekranH * 0.3,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Image.network(
-                  'https://image.tmdb.org/t/p/original' +
-                      flist[index].posterPath,
-                ),
-              ),
+            Icon(
+              Icons.menu,
+              color: Colors.purple[700],
             ),
-            Container(
-              child: Text(
-                flist[index].name,
-                style: TextStyle(
-                  color: Colors.deepPurple[300],
-                ),
+            SizedBox(
+              width: 30,
+            ),
+            Text(
+              "Trend Films",
+              style: TextStyle(
+                color: Colors.purple[700],
               ),
             ),
           ],
         ),
-        staggeredTileBuilder: (index) => new StaggeredTile.count(
-            index != 0 ? 2 : 4, index * 2 / 2 != index ? 2 : 1),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
+        iconTheme: IconThemeData(
+          color: Colors.green,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      // drawer: Drawer(),
+      body: Container(
+        child: FutureBuilder(
+          future: getState(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      Text('Loading...'),
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                //itemCount: snapshot.data.len,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Stack(
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(5),
+                          width: index / 2 * 2 == index ? ekranW : ekranW * .3,
+                          height: ekranH * .3,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  'https://image.tmdb.org/t/p/original' +
+                                      snapshot.data[index].posterPath,
+                                ),
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(15),
+                          child: Text(
+                            snapshot.data[index].name,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              decorationColor: Colors.pink,
+                              decorationStyle: TextDecorationStyle.solid,
+                              backgroundColor: Colors.black54,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
